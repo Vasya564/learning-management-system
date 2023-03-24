e2ee68b39268de66475a8170bc309d59a092c90a
Vasya564 <dudlavasil@gmail.com>
2023-03-24 18:50:34 +0200

    client setup, sidenav, router setup

---
+++ client/src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// import pages
+import Sidebar from './pages/Sidebar/Sidebar';
+import Error from './pages/Error/Error';
+import Home from './pages/Home/Home';
+
+// import global styles
+import './global.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Sidebar />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "c",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
