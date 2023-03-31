c7380bbb41896a42f40d5aad42c466bd44a82c09
Vasya564 <dudlavasil@gmail.com>
2023-04-01 01:22:48 +0300

    Change error page, add styles

--- client/src/pages/Error/Error.jsx
+++ client/src/pages/Error/Error.jsx
@@ -1,7 +1,11 @@
+import './Error.scss'
+import { BsFillExclamationTriangleFill } from 'react-icons/bs'
+
 const Error = () => {
     return (
-        <div>
-            <h1>Oops something went wrong</h1>
+        <div className='error__wrapper'>
+            <BsFillExclamationTriangleFill size={'3rem'}/>
+            <h1 className='error__mssg'>Такої сторінки не існує</h1>
         </div>
     );
 }
