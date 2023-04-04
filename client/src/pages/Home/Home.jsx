8b42948fd92a515c7965b94f96048c794a03a4b3
Vasya564 <dudlavasil@gmail.com>
2023-04-04 20:02:46 +0300

    Add filter by status, fetch course data from server

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,44 +1,60 @@
 import './Home.scss'
-import { BsFillChatTextFill } from 'react-icons/bs'
+import { useEffect, useState } from 'react';
+
+// components
+import CourseCard from '../../components/CourseCard/CourseCard';
 
 const Home = () => {
+    const [courses, setCourses] = useState(null);
+    const [filterValue, setFilterValue] = useState("");
+    const [filteredCourses, setFilteredCourses] = useState([]);
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            const response = await fetch('http://localhost:4000/api/courses')
+            const json = await response.json()
+
+            if (response.ok) {
+                setCourses(json)
+            }
+        }
+
+        fetchCourses()
+    }, [])
+
+    useEffect(() => {
+        if (courses){
+            const newFilteredCourses = courses.filter((course) => {
+                if (filterValue === "") {
+                    return true;
+                } else {
+                    return course.status === parseInt(filterValue);
+                }
+            });
+            setFilteredCourses(newFilteredCourses);
+        }
+    }, [courses, filterValue])
+    
     return (
         <> 
             {/* Page header with filter */}
             <header className='content__header'>
                 <h1>Мої курси</h1>
-                <select>
-                    <option>Всі статуси</option>
-                    <option>Активні</option>
-                    <option>Завершені</option>
-                    <option>Повторний курс</option>
+                <select 
+                    value={filterValue} 
+                    onChange={(e) => setFilterValue(e.target.value)}
+                >
+                    <option value="">Всі статуси</option>
+                    <option value="1">Активні</option>
+                    <option value="2">Завершені</option>
+                    <option value="3">Повторний курс</option>
                 </select>
             </header>
             {/* Courses wrapper and courses */}
             <section className='content__courses'>
-                <div className='content__course'>
-                    <div className='content__course__status'>
-                        <BsFillChatTextFill />
-                        <p>Активний</p>
-                    </div>
-                    <div className='content__course__main'>
-                        <div className='content__course__header'>
-                            <div>
-                                <h3>Назва курсу</h3>
-                                <p>Спеціалізація</p>
-                            </div>
-                            <button>Перейти до курсу</button>
-                        </div>
-                        <div className='content__course__footer'>
-                            <hr />
-                            <div className='content__course__teacher'>
-                                {/* <img /> */}
-                                <div></div>
-                                <p>Шевченко Тарас</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {filteredCourses.map((course) => (
+                    <CourseCard key={course._id} course={course}/>
+                ))}
             </section>
         </>
     );
