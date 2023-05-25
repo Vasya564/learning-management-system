fe774d2d0a0ee21ed65b2352a8dc2d0083282bbc
Vasya564 <dudlavasil@gmail.com>
2023-05-25 18:04:16 +0300

    Update course controller + routes

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -1,5 +1,7 @@
 const Course = require('../models/courseModel')
 const mongoose = require('mongoose')
+const fs = require('fs');
+const path = require('path');
 
 // get all courses
 const getCourses = async (req, res) => {
@@ -73,10 +75,106 @@ const updateCourse = async (req, res) => {
     res.status(200).json(course)
 }
 
+const updateResources = async (req, res) => {
+    const courseId = req.params.id;
+    const topic = req.body.topic;
+    const files = req.files;
+
+    // Find the course by courseId in the database
+    try {
+        // Find the course by courseId in the database
+        const course = await Course.findById(courseId);
+    
+        if (!course) {
+          return res.status(404).json({ error: 'Course not found' });
+        }
+    
+        // Create an array to store the file information
+        const fileData = [];
+    
+        // Process each file and store its information
+        for (let i = 0; i < files.length; i++) {
+          const file = files[i];
+    
+          const fileInfo = {
+            originalName: file.originalname,
+            filename: file.filename,
+            path: `/uploads/${file.filename}`,
+            mimetype: file.mimetype
+          };
+    
+          fileData.push(fileInfo);
+        }
+    
+        // Create a new block object with the received topic and file data
+        const newBlock = {
+          topic: topic,
+          files: fileData
+        };
+    
+        // Add the new block to the resources array
+        course.resources.push(newBlock);
+    
+        // Update the course and retrieve the updated document
+        const updatedCourse = await Course.findOneAndUpdate(
+          { _id: courseId },
+          { $set: { resources: course.resources } },
+          { new: true }
+        );
+    
+        res.status(200).json(updatedCourse);
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server error' });
+      }
+}
+
+const deleteBlockFromResources = async (req, res) => {
+    try {
+        const { id, blockId } = req.params;
+    
+        // Find the course by ID
+        const course = await Course.findById(id);
+    
+        if (!course) {
+            return res.status(404).json({ error: 'Course not found' });
+        }
+    
+        if (blockId < 0 || blockId >= course.resources.length) {
+            return res.status(400).json({ error: 'Invalid block index' });
+        }
+
+        const block = course.resources[blockId];
+
+        // Delete the associated files from the server's file system
+        block.files.forEach((file) => {
+            const filePath = path.join(__dirname, '../uploads', file.filename);
+            fs.unlink(filePath, (err) => {
+              if (err) {
+                console.error(err);
+              }
+            });
+          });
+  
+        // Remove the block at the specified index
+        course.resources.splice(blockId, 1);
+    
+        // Save the updated course
+        const updatedCourse = await course.save();
+    
+        res.status(200).json(updatedCourse);
+        } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+        }
+  };
+
 module.exports = {
     getCourses,
     getCourse,
     createCourse,
     deleteCourse,
-    updateCourse
+    updateCourse,
+    updateResources,
+    deleteBlockFromResources
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const path = require('path');
 const coursesRoutes = require('./routes/courses')
 const userRoutes = require('./routes/user')
 
@@ -18,9 +19,15 @@ app.use((req, res, next) => {
     next()
 })
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.text({ defaultCharset: 'utf-8' }));
+
 // routes
 app.use('/api/courses', coursesRoutes)
 app.use('/api/user', userRoutes)
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
